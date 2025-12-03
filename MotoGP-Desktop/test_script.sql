09f4d5922bc260f33a68e6016cf8644205aa98eb
UO301831 <uo301831@uniovi.es>
2025-12-03 14:49:53 +0100

    Incluido script de prueba

--- MotoGP-Desktop/test_script.sql
+++ MotoGP-Desktop/test_script.sql
@@ -1,6 +1,6 @@
 drop table if exists user_info
 create table user_info(
-    codigo varchar(12) primary key not null,
+    id varchar(12) primary key not null,
     profesion varchar(48) not null,
     edad integer check (edad >= 0),
     genero varchar(1) check (genero like 'M' or genero like 'F'),
@@ -9,8 +9,20 @@ create table user_info(
 
 drop table if exists test_info
 create table test_info(
-    
-    tiempo_tardado ... not null,
+    idUsuario varchar(12) primary key not null,
+    dispositivo varchar(48) not null check (LOWER(dispotivo) like 'ordenador' OR LOWER(dispositivo) like 'tableta' OR LOWER(dispositivo) like 'telefono'),
+    tiempo_tardado int not null,
     completado boolean not null,
-    
+    comentarios varchar(512) not null,
+    propuestas varchar(256) not null,
+    valoracion int check (valoracion >= 0 AND valoracion <= 10),
+    CONSTRAINT FK_USUARIO_TEST_INFO FOREIGN KEY (idUsuario) REFERENCES user_info(id) 
+);
+
+drop table if exists consideration
+create table consideration(
+    idUsuario varchar(12) not null,
+    comentarios varchar(512) not null,
+    CONSTRAINT PK_CONSIDERATION_IDUSUARIO PRIMARY KEY (idUsuario),
+    CONSTRAINT FK_CONSIDERATION_IDUSUARIO FOREIGN KEY (idUsuario) REFERENCES user_info(id)
 );
